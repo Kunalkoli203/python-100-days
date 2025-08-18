b61c433d08ced7ea59e853aaeec3030dc79f59aa
kunalkoli203 <kolikunal336@gmail.com>
2025-08-19 01:30:05 +0530

    strings

---
+++ day11.py
@@ -0,0 +1,22 @@
+#STRINGS IN PYTHON.in python string are enclosed in single aur double or triple quotes
+string = "kunal"
+string1='jayesh'
+string2="'mahesh'"
+
+print(string)
+print(string1)
+print(string2)
+
+name="harry"
+print("Hello "  +  name)  #strings are always concate so result is hello harry
+
+
+#strings have indexes and that starts from 0 as usual
+
+print(name[0])  #it prints first character of name string
+print(name[1])  #it prints second character of name string
+
+#looping in strings 
+for character in name:
+    print(character)
+
